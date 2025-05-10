2c7db490c449152eb149af78fe4308d5a7dbab7d
Sarnitha A D <sharnithadhandapani@gmail.com>
2025-05-10 12:32:24 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Backend running at http://localhost:${PORT}`);
 });
